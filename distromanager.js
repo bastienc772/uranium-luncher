f474c7a813e0241da71c0630cf498915a62dc2f8
bastienc772 <chiribastien9@gmail.com>
2026-06-17 20:15:50 +0200

    Add server configuration and metadata to distromanager

--- distromanager.js
+++ distromanager.js
@@ -1,15 +1,39 @@
-const { DistributionAPI } = require('helios-core/common')
-
-const ConfigManager = require('./configmanager')
-
-exports.REMOTE_DISTRO_URL = 'https://raw.githubusercontent.com/bastienc772/uranium-luncher/main/distribution.json'
-
-const api = new DistributionAPI(
-    ConfigManager.getLauncherDirectory(),
-    null, // Injected forcefully by the preloader.
-    null, // Injected forcefully by the preloader.
-    exports.REMOTE_DISTRO_URL,
-    false
-)
-
-exports.DistroAPI = api
+{
+    "version": "1.0.0",
+    "discord": {
+        "clientId": "0000000000000000000",
+        "smallImageText": "Uranium PVP",
+        "smallImageKey": "seal-circle"
+    },
+    "rss": "",
+    "servers": [
+        {
+            "id": "uranium-pvp-1.20.1",
+            "name": "Uranium PVP",
+            "description": "Serveur officiel Uranium PVP - Minecraft Forge 1.20.1",
+            "icon": "",
+            "version": "1.0.0",
+            "address": "whoever-units.gl.joinmc.link",
+            "minecraftVersion": "1.20.1",
+            "discord": {
+                "shortId": "UraniumPVP",
+                "largeImageText": "Uranium PVP Server",
+                "largeImageKey": "server-prod"
+            },
+            "mainServer": true,
+            "autoconnect": true,
+            "modules": [
+                {
+                    "id": "net.minecraftforge:forge:1.20.1-47.4.10",
+                    "name": "Minecraft Forge",
+                    "type": "ForgeHosted",
+                    "artifact": {
+                        "size": 0,
+                        "MD5": "",
+                        "url": "https://maven.minecraftforge.net/net/minecraftforge/forge/1.20.1-47.4.10/forge-1.20.1-47.4.10-installer.jar"
+                    }
+                }
+            ]
+        }
+    ]
+}
